f3bf3675db2818b34a8d50116f39d587cb77484e
Andrew Kennedy <akennedy2292@gmail.com>
2013-10-27 14:07:16 -0400

    moved some shit around

--- app/js/models/level.js
+++ app/js/models/level.js
@@ -2,6 +2,16 @@ define(['backbone'], function(Backbone) {
     return Backbone.Model.extend({
         initialize: function() {
             console.log("model level", this.get('levelNumber'));
+        },
+        createPoops: function(num, rate, speed, delay) {
+            var lastPoop = delay || 0;
+            for (var i = 0; i < num; i++) {
+                lastPoop += rate;
+                setTimeout(function() {
+                    var poop = Crafty.e('Poop');
+                    poop.movementSpeed = speed;
+                }, lastPoop);
+            }
         }
     });
 });
--- app/js/scenes/level1.js
+++ app/js/scenes/level1.js
@@ -1,26 +1,16 @@
 define(['backbone'], function(Backbone) {
-    function createPoops(num, rate, speed, delay) {
-        var lastPoop = delay || 0;
-        for (var i = 0; i < num; i++) {
-            lastPoop += rate;
-            setTimeout(function() {
-                var poop = Crafty.e('Poop');
-                poop.movementSpeed = speed;
-            }, lastPoop);
-        }
-    }
     return Backbone.View.extend({
         initialize: function() {
             Crafty.scene('Game', function() {
-                createPoops(15, 80, 5, 150);
-                createPoops(10, 100, 3, 300);
+                this.model.createPoops(15, 80, 5, 150);
+                this.model.createPoops(10, 100, 3, 300);
 
                 Crafty.e('Planet');
                 Crafty.e('Player');
                 Crafty.e('Crosshair');
                 var monkeys = Crafty.e('Actor, Image').attr({x: BGJ.GameModel.get('width')/2 - 100, y: 70, z: -1, w: 192, h: 224}).image('assets/img/monkeys.png');
 
-            });
+            }.bind(this));
             Crafty.scene('Game');
         }
     });
